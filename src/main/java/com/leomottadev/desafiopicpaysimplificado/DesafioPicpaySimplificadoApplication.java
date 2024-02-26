0d456f95c27c808afbe7ad8dba2094f939564504
Leonardo Motta de Oliveira <leonardomotta@ucl.br>
2024-02-26 09:53:16 -0300

    Initial setup

---
+++ src/main/java/com/leomottadev/desafiopicpaysimplificado/DesafioPicpaySimplificadoApplication.java
@@ -0,0 +1,13 @@
+package com.leomottadev.desafiopicpaysimplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPicpaySimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPicpaySimplificadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leomottadev/desafiopicpaysimplificado/DesafioPicpaySimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leomottadev.desafiopicpaysimplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPicpaySimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
